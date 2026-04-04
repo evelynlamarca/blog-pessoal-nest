3f59b176affbcc2c3cf6c86dd2c5de23ee0c5013
evelynlamarca <evelynkarina15@gmail.com>
2026-04-04 09:31:11 -0300

    feat: configuracao de deploy final

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,33 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { Tema } from './tema/entities/tema.entity';
+import { Usuario } from './usuario/entities/usuario.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
-import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Postagem } from './postagem/entities/postagem.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(), // 
     TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'db_blogpessoal',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
-}),
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',     //
+      port: Number(process.env.DB_PORT) || 3306,    // 
+      username: process.env.DB_USER || 'root',      // 
+      password: process.env.DB_PASSWORD || 'root',  // 
+      database: process.env.DB_NAME || 'db_blogpessoal', // 
+      entities: [Postagem, Tema, Usuario],
+      synchronize: true, // 
+    }),
     PostagemModule,
     TemaModule,
-    AuthModule,
     UsuarioModule,
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
