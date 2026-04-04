592c9865c1a926f908081aeb5b3ad0f60a5c4221
evelynlamarca <evelynkarina15@gmail.com>
2026-04-04 10:02:47 -0300

    feat: instalando driver pg e configurando prodservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,15 @@ import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
+import { ProdService } from './data/services/prod.service'; // 
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // 
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',     //
-      port: Number(process.env.DB_PORT) || 3306,    // 
-      username: process.env.DB_USER || 'root',      // 
-      password: process.env.DB_PASSWORD || 'root',  // 
-      database: process.env.DB_NAME || 'db_blogpessoal', // 
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true, // 
+    ConfigModule.forRoot(),
+    // 
+    TypeOrmModule.forRootAsync({
+        useClass: ProdService, // 
+        imports: [ConfigModule],
     }),
     PostagemModule,
     TemaModule,
